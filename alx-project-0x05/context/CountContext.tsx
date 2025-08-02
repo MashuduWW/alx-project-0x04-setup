00f432f290ff594eba49d782b30250734a31e424
Mashudu Molema <mrkaymp@gmail.com>
2025-08-02 18:40:45 +0200

    Amended CountContext to explicitly use increment and decrement.

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -1,19 +1,23 @@
-import { createContext, useContext,  useState, ReactNode } from "react"
+import { createContext, useContext, useState, ReactNode } from "react"
 
 interface CountContextProps {
   count: number
-  increment: () => void
-  decrement: () => void
+  increment: () => void;
+  decrement: () => void;
 }
 
 export const CountContext = createContext<CountContextProps | undefined>(undefined)
 
-export const CountProvider = ({ children }: { children: ReactNode}) => {
-
+export const CountProvider = ({ children }: { children: ReactNode }) => {
   const [count, setCount] = useState<number>(0)
 
-  const increment = () => setCount((count ) =>count + 1)
-  const decrement = () => setCount((count) => count > 0 ? count - 1 : 0)
+  const increment: () => void = () => {
+    setCount((count) => count + 1)
+  }
+
+  const decrement: () => void = () => {
+    setCount((count) => (count > 0 ? count - 1 : 0))
+  }
 
   return (
     <CountContext.Provider value={{ count, increment, decrement }}>
@@ -22,13 +26,11 @@ export const CountProvider = ({ children }: { children: ReactNode}) => {
   )
 }
 
-
-
 export const useCount = () => {
   const context = useContext(CountContext)
 
   if (!context) {
-    throw new Error("useCount must be within a Count Provider")
+    throw new Error("useCount must be within a CountProvider")
   }
 
   return context
